dd3d9d37d0ca09f90cdb2f606ce8b701243bb17e
Steve Bourgoin <stiv.aw@gmail.com>
2023-01-13 16:24:28 -0500

    Add App header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,53 @@
-import { Container, Header, Title } from "@mantine/core";
+import {
+  Badge,
+  Container,
+  createStyles,
+  Group,
+  Header,
+  Text,
+} from "@mantine/core";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { type ComponentProps } from "react";
 
 interface Props extends ComponentProps<typeof Container> {
   children: React.ReactNode;
 }
 
+const LINKS = {
+  search: { href: "/search", label: "Search" },
+  books: { href: "/", label: "Books" },
+};
+
 export function Layout({ children, ...containerProps }: Props) {
+  const { classes, cx } = useStyles();
+  const router = useRouter();
+
   return (
     <>
       <Header height={60} pos="sticky" zIndex={900}>
-        <Container
-          sx={{ display: "flex", alignItems: "center", height: "100%" }}
-        >
-          <Title size={24}>Seshat - My virtual bookshelf</Title>
+        <Container className={classes.header}>
+          <Group>
+            <Text component={Link} href="/" size="lg">
+              Seshat
+            </Text>
+
+            <Badge size="sm">Alpha</Badge>
+          </Group>
+
+          <Group spacing="md">
+            {Object.entries(LINKS).map(([, link]) => (
+              <Link
+                className={cx(classes.link, {
+                  [classes.linkActive]: isActive(link.href, router.pathname),
+                })}
+                href={link.href}
+                key={link.href}
+              >
+                {link.label}
+              </Link>
+            ))}
+          </Group>
         </Container>
       </Header>
 
@@ -20,3 +55,46 @@ export function Layout({ children, ...containerProps }: Props) {
     </>
   );
 }
+
+function isActive(href: string, pathname: string) {
+  if (href === "/") return pathname === href;
+  return pathname.startsWith(href);
+}
+
+const useStyles = createStyles((theme) => ({
+  header: {
+    backgroundColor: theme.colors.dark[9],
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "100%",
+  },
+
+  link: {
+    display: "block",
+    lineHeight: 1,
+    padding: "8px 12px",
+    borderRadius: theme.radius.sm,
+    textDecoration: "none",
+    color: theme.colors.gray[7],
+    fontSize: theme.fontSizes.sm,
+    fontWeight: 500,
+
+    "&:hover": {
+      backgroundColor: theme.colors.dark[6],
+    },
+  },
+
+  linkActive: {
+    "&, &:hover": {
+      backgroundColor: theme.fn.variant({
+        variant: "light",
+        color: theme.primaryColor,
+      }).background,
+      color: theme.fn.variant({
+        variant: "light",
+        color: theme.primaryColor,
+      }).color,
+    },
+  },
+}));
