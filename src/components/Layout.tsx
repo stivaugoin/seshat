400f4a6ba9d75417241db4e12e77e4c545546a1e
Steve Bourgoin <stiv.aw@gmail.com>
2023-01-14 17:57:07 -0500

    Replace search label by icon

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -20,7 +20,7 @@ interface Props extends ComponentProps<typeof Container> {
 }
 
 const LINKS = {
-  search: { href: "/search", label: "Search" },
+  search: { href: "/search", icon: <SearchIcon /> },
   books: { href: "/", label: "Books" },
 };
 
@@ -67,7 +67,8 @@ export function Layout({ children, ...containerProps }: Props) {
                 href={link.href}
                 key={link.href}
               >
-                {link.label}
+                {"icon" in link && link.icon}
+                {"label" in link && link.label}
               </Link>
             ))}
           </Group>
@@ -121,3 +122,20 @@ const useStyles = createStyles((theme) => ({
     },
   },
 }));
+
+function SearchIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      style={{ height: "1rem", width: "1rem" }}
+    >
+      <path
+        fillRule="evenodd"
+        d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
