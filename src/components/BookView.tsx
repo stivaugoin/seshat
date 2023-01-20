1c933eacca8362c04e7e6d055b0da5cc091c1c9a
Steve Bourgoin <stiv.aw@gmail.com>
2023-01-19 21:15:17 -0500

    Reuse BookView in search list

--- src/components/BookView.tsx
+++ src/components/BookView.tsx
@@ -1,8 +1,10 @@
 import { Badge, Group, Rating, Stack, Text, Title } from "@mantine/core";
 import type { Book } from "@prisma/client";
+import type { inferRouterOutputs } from "@trpc/server";
+import type { AppRouter } from "../server/api/router";
 
 interface Props {
-  book: Book;
+  book: Book | inferRouterOutputs<AppRouter>["search"][0]["book"];
 }
 
 export function BookView({ book }: Props) {
@@ -10,9 +12,9 @@ export function BookView({ book }: Props) {
     <Stack spacing="sm">
       <Title order={3}>{book.title}</Title>
 
-      <Group>
+      <Group spacing="xs">
         {book.authors.map((author) => (
-          <Badge color="gray" variant="outline" key={author}>
+          <Badge color="gray" variant="outline" key={author} radius="xs">
             {author}
           </Badge>
         ))}
@@ -22,25 +24,27 @@ export function BookView({ book }: Props) {
         {book.publishedYear} &middot; {book.pages} pages
       </Text>
 
-      <Group grow position="apart">
-        {book.rating ? (
-          <Rating readOnly value={book.rating} />
-        ) : (
-          <Text color="dimmed" italic>
-            Not rated yet
-          </Text>
-        )}
+      {"rating" in book && "readYear" in book && (
+        <Group grow position="apart">
+          {book.rating ? (
+            <Rating readOnly value={book.rating} />
+          ) : (
+            <Text color="dimmed" italic>
+              Not rated yet
+            </Text>
+          )}
 
-        {book.readYear ? (
-          <Text align="right" color="dimmed">
-            Read in {book.readYear}
-          </Text>
-        ) : (
-          <Text align="right" color="dimmed" italic>
-            Not read yet
-          </Text>
-        )}
-      </Group>
+          {book.readYear ? (
+            <Text align="right" color="dimmed">
+              Read in {book.readYear}
+            </Text>
+          ) : (
+            <Text align="right" color="dimmed" italic>
+              Not read yet
+            </Text>
+          )}
+        </Group>
+      )}
     </Stack>
   );
 }
