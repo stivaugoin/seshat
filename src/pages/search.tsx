c236f27bb9474c72a5b5d5cb4eeaedefa411ac15
Steve Bourgoin <stiv.aw@gmail.com>
2023-01-19 21:15:17 -0500

    Reuse BookView in search list

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,10 +1,11 @@
-import { Button, Code, Loader, Stack, TextInput } from "@mantine/core";
+import { Button, Divider, Loader, Stack, TextInput } from "@mantine/core";
 import { showNotification } from "@mantine/notifications";
 import type { inferRouterOutputs } from "@trpc/server";
 import { type NextPage } from "next";
 import { useRouter } from "next/router";
 import type { FormEvent } from "react";
 import { AlertError } from "../components/AlertError";
+import { BookView } from "../components/BookView";
 import { Layout } from "../components/Layout";
 import type { AppRouter } from "../server/api/router";
 import { api } from "../utils/api";
@@ -77,22 +78,33 @@ function Result({ data }: { data: inferRouterOutputs<AppRouter>["search"] }) {
 
       {data.map(({ book, source }) => (
         <Stack key={book.isbn}>
-          <Code block>{JSON.stringify(book, null, 4)}</Code>
+          <BookView book={book} />
 
           {source === "database" && (
-            <Button onClick={() => router.push(`/${book?.isbn}`)}>
+            <Button
+              color="gray"
+              leftIcon={<IconView />}
+              onClick={() => router.push(`/${book?.isbn}`)}
+              variant="outline"
+              radius="xl"
+            >
               View in bookshelf
             </Button>
           )}
 
           {source === "googleApi" && (
             <Button
+              leftIcon={<IconAdd />}
               loading={addBook.isLoading}
               onClick={() => handleClickAdd({ isbn: book.isbn })}
+              radius="xl"
+              variant="outline"
             >
               Add to bookshelf
             </Button>
           )}
+
+          <Divider />
         </Stack>
       ))}
     </>
@@ -101,4 +113,47 @@ function Result({ data }: { data: inferRouterOutputs<AppRouter>["search"] }) {
 
 function isIsbn(value: string) {
   return value.length === 13 && !isNaN(Number(value));
+}
+
+function IconView() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      style={{ height: "1rem", width: "1rem" }}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+      />
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+      />
+    </svg>
+  );
+}
+
+function IconAdd() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      style={{ height: "1rem", width: "1rem" }}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 4.5v15m7.5-7.5h-15"
+      />
+    </svg>
+  );
 }
